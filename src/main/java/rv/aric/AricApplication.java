b1052d316bb14306865a80b0aacf5eb907d7b2d9
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-17 22:36:02 -0300

    Initial commit #0

---
+++ src/main/java/rv/aric/AricApplication.java
@@ -0,0 +1,13 @@
+package rv.aric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rv/aric/AricApplicationTests.java
@@ -0,0 +1,13 @@
+package rv.aric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
