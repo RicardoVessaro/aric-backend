0da70ff7b403721606713874cc71300732c2c981
RicardoVessaro <ricardolvessaro@gmail.com>
2023-11-09 15:14:55 -0300

    Initial backend setup #2

---
+++ src/main/java/rv/aric/system/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package rv.aric.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(configurer -> {
+            configurer.requestMatchers(HttpMethod.GET, "/system/**").permitAll();
+        });
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/rv/aric/system/rest/SystemController.java
@@ -0,0 +1,15 @@
+package rv.aric.system.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+
+    @GetMapping("/check")
+    public String check() {
+        return "OK";
+    }
+}
